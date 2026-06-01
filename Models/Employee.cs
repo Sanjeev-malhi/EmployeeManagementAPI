e6fbc348945407f20e2742c6e2f4b6bd056d29f8
Sanjeev Malhi <sanjeevmalhi9145@gmail.com>
2026-06-01 23:24:29 +0530

    Add AppSetting Files

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EmployeeManagementAPI.Models;
 
 public class Employee
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Range(1000, 1000000)]
     public decimal Salary { get; set; }
 }
